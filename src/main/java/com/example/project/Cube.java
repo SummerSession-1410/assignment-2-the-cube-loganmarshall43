dfeacb02e67728a33586f1d075ca13e756237d7d
loganmarshall43 <135401419+loganmarshall43@users.noreply.github.com>
2023-08-07 16:56:39 -0600

    Update Cube.java

--- src/main/java/com/example/project/Cube.java
+++ src/main/java/com/example/project/Cube.java
@@ -1,12 +1,40 @@
-package com.example.project;
+public class RubiksCube {
+    private char[][][] cube;
 
-public class Cube {
+    public RubiksCube() {
+        cube = new char[6][3][3];
+        initializeCube();
+    }
 
-	public static void main(final String[] args) {
-		System.out.println("Not the right string, tests will fail!");
-	//hi there
-	
-	}
+    private void initializeCube() {
+        char[] colors = {'r', 'b', 'o', 'g', 'y', 'w'};
+        for (int face = 0; face < 6; face++) {
+            char color = colors[face];
+            for (int row = 0; row < 3; row++) {
+                for (int col = 0; col < 3; col++) {
+                    cube[face][row][col] = color;
+                }
+            }
+        }
+    }
 
+    public void printCube() {
+        for (int face = 0; face < 6; face++) {
+            for (int row = 0; row < 3; row++) {
+                for (int col = 0; col < 3; col++) {
+                    System.out.print(cube[face][row][col]);
+                    if (col < 2) {
+                        System.out.print("|");
+                    }
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        RubiksCube cube = new RubiksCube();
+        cube.printCube();
+    }
 }
-
